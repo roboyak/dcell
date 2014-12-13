7733d5bf9c6c9fd485fd273fdcec71946816d955
Shawn Cunningham <shawn@layer3dev.com>
2014-12-13 10:48:49 -0800

    debug refactor

--- examples/yakmore.rb
+++ examples/yakmore.rb
@@ -6,11 +6,42 @@ vizor_node = DCell::Node["vizor"]
 
 puts "Waking crawler, hidden dragon.."
 
+class Page
+
+	attr_reader :url
+
+	def initialize(url, params = {})
+		  puts "Initializing page with url: #{url}"
+	  @url = url
+	end
+
+	def crawl
+		  puts "Crawling url: #{url}"
+	end
+
+	def queue_links
+	  puts "Q'ing links"
+	end
+
+	def save_categories
+	  puts "Saving categories"
+	end
+
+	def to_html
+	  puts "to_html"
+	end
+
+	def to_json
+	  puts "to_json"
+	end
+end
+
+
 loop do
   link = vizor_node[:vizor].get_next_link
 
   unless link.empty?
-	  page = Slurp::Page.new(link)
+	  page = Page.new(link)
 
 	  page.crawl
 
@@ -27,36 +58,3 @@ loop do
 
    sleep 1
 end
-
-module Slurp
-  class Page
-
-    attr_reader :url
-
-    def initialize(url, params = {})
-   	  puts "Initializing page with url: #{url}"
-      @url = url
-    end
-
-    def crawl
-   	  puts "Crawling url: #{url}"
-    end
-
-    def queue_links
-      puts "Q'ing links"
-    end
-
-    def save_categories
-      puts "Saving categories"
-    end
-
-    def to_html
-      puts "to_html"
-    end
-
-    def to_json
-      puts "to_json"
-    end
-  end
-end
-
